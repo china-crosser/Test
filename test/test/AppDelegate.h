7308836561bdee90e3bae1e0e97ddeef7650dd72
china_crosser <china_crosser@163.com>
2013-11-08 23:28:19 +0800

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by crosser on 13-11-8.
+//  Copyright (c) 2013年 crosser. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ test/test/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  test
+//
+//  Created by crosser on 13-11-8.
+//  Copyright (c) 2013年 crosser. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
